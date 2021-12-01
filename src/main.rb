aa161bd4c4715fa0facada4d7d2e5f4deb1ffc5e
FluroAsh <workashleythompson@gmail.com>
2021-12-01 17:16:07 +1100

    Adds comments with general outline of scope/features

---
+++ src/main.rb
@@ -0,0 +1,30 @@
+## GOAL: Implement an application to receive and query data
+## From Movie Database via an API (https://rapidapi.com/blog/how-to-use-imdb-api/)
+
+### -- GEM DEPENDICES: Colorize, 
+
+    ## FEATURES:
+    # 1. SEARCH OPTIONS:
+    # 1.1 Movie Title Name
+    # 1.2 Year
+    # 1.3 Metascore Rating
+    # 1.4 IMDB Rating
+    # 1.5 Release Date
+    # 1.6 Runtime
+    # 1.7 Genre
+    # 1.8 Director(s)
+    # 1.9 Writer(s)
+    
+    # 2. Provide movie 'tile' output in terminal
+    # 2.1.Title
+    # 2.2 Year
+    # 2.3 Runtime '#{1} hr #{42} min'
+    # 2.4 Genre
+    # 2.5 Directors 'Directed By: '
+    # 2.6 Writers 'Written By: '
+    # 2.7 Actors
+    # 2.8 Plot
+    # 2.10 Awards
+
+    # 3. Add to watchlist option
+    # 3.1 Writes to text-file with correct format (1. Movie Title, Year, Runtime, Genre, Director)
